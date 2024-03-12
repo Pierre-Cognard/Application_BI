94ff2366ff2a479f37953590fa517a9ae011c027
anthony-vitrant <anthony.vitrant@alumni.univ-avignon.fr>
2024-03-12 17:37:34 +0100

    analyse ultime des sirets

--- Script python/analyse_nombre.py
+++ Script python/analyse_nombre.py
@@ -1,7 +1,9 @@
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
+import requests
 from dict_variables import *
+import time
 
 def lire_fichier_csv(nom_fichier):
     donnees = []
@@ -30,6 +32,32 @@ def generer_graphique(data, champ, log):
     valeurs_analysees, min_val, max_val = analyser_valeurs(data, champ)
     for valeur, occurence in valeurs_analysees.items():
         print(f"Valeur: {valeur}, Occurences: {occurence}")
+    
+    if champ == "siret":
+        validator = Siret()
+        valid_count = 0
+        invalid_count = 0
+        invalid_sirets = []
+        for d in data:
+            if d[champ] != "" and validator.is_valid(d[champ]):
+                valid_count += 1
+            elif d[champ] != "":
+                invalid_count += 1
+                invalid_sirets.append(d[champ])
+
+        
+        total_count = valid_count + invalid_count
+        print(f"Pourcentage de SIRET valides : {valid_count / total_count * 100:.2f}%")
+        print(f"Pourcentage de SIRET invalides : {invalid_count / total_count * 100:.2f}%")
+        # Stockage des SIRET invalides dans un fichier texte
+        with open("siret_invalides.txt", "w") as file:
+            file.write("SIRET invalides :\n")
+            for siret in invalid_sirets:
+                file.write(siret + "\n")
+        
+        true_count, false_count = count_validity(invalid_sirets)
+        print("Nombre de SIRET valides par API:", true_count)
+        print("Nombre de SIRET invalides par API:", false_count)
 
     plage_de_donnees = max_val - min_val
 
@@ -39,7 +67,6 @@ def generer_graphique(data, champ, log):
     # Calculez la largeur en fonction de la plage de données
     largeur = largeur_en_pourcentage / 100 * plage_de_donnees
 
-    print("largeur" , largeur)
 
     # Maintenant, utilisez cette largeur dans la fonction plt.bar()
     plt.bar(valeurs_analysees.keys(), valeurs_analysees.values(), width=largeur)
@@ -51,6 +78,45 @@ def generer_graphique(data, champ, log):
         plt.ylabel('Occurences')
     plt.title('Répartition des valeurs de ' + champ)
     plt.show()
+
+class Siret:
+    SIRET_LENGTH = 14
+
+    def is_valid(self, siret: str) -> bool:
+        siret = siret.strip() if siret else ""
+        if not siret.isdigit() or len(siret) != self.SIRET_LENGTH:
+            return False
+
+        total = 0
+        for i in range(self.SIRET_LENGTH):
+            temp = int(siret[i]) * (2 if i % 2 == 0 else 1)
+            temp = temp - 9 if temp > 9 else temp
+            total += temp
+
+        return total % 10 == 0
+
+def is_siret_valid(siret):
+    url = f"https://api.insee.fr/entreprises/sirene/V3/siret/{siret}"
+    headers = {
+        "Authorization": "Bearer 8164183c-73ab-3977-b9a3-18dfdb3f3dc8"
+    }
+    response = requests.get(url, headers=headers)
+    time.sleep(0.5)  # Pause de 2 secondes max api
+    print(response)
+    if response.status_code == 200:
+        data = response.json()
+        return not data.get('error', False)
+    return False
+
+def count_validity(sirets):
+    true_count = 0
+    false_count = 0
+    for siret in sirets:
+        if is_siret_valid(siret):
+            true_count += 1
+        else:
+            false_count += 1
+    return true_count, false_count
 
 """if __name__ == "__main__":
     fichier_csv = "../Foppa/Agents.csv"
